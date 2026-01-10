aeeea10729964144842384fecbcf8b032eb747dc
shochcho <shochcho46@gmail.com>
2026-01-10 21:39:11 +0600

    modify done

--- Modules/Product/resources/views/reports/order-profitreport.blade.php
+++ Modules/Product/resources/views/reports/order-profitreport.blade.php
@@ -213,13 +213,13 @@
 
 @endsection
 
-@push('styles')
+@push('custome-css')
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" rel="stylesheet" />
 @endpush
 
-@push('scripts')
+@push('custome-js')
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
